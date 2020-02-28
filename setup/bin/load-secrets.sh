eea81641a8c7900f8cbea0ede9d3b6521e11e87f
Stephen Ball <stephen@rebelinblue.com>
2020-02-28 18:42:02 +0000

    Remove registry

--- setup/bin/load-secrets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-REPO_ROOT=$(git rev-parse --show-toplevel)
-source "$REPO_ROOT/setup/.env"
-
-kubectl -n registry create secret generic docker-registry-auth --from-literal=htpasswd=$(htpasswd -nbB $REGISTRY_USERNAME $REGISTRY_PASSWORD)
